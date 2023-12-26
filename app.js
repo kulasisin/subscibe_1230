76b2e860dd1651cc07747e17a989f3eb87bdbfa7
kulasisin <107702039@mail2.nccu.tw>
2023-12-26 16:09:26 +0800

    3

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const app = express();
 const Joi = require("@hapi/joi");
 const movies = require("./movies");
 
-app.use(express.json());
+// app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res.send("歡迎來到永夜安安工作室！！探險火寶現正熱映中！！1226");
+// });
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.send("歡迎來到永夜安安工作室！！探險火寶現正熱映中！！1226");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 const port = process.env.PORT || "5000";
