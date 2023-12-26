d55b591747944b4299d79de77befc60255652aa3
kulasisin <107702039@mail2.nccu.tw>
2023-12-26 19:37:15 +0800

    add bs scss

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
+import "./scss/stylesheets/style.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
+const bootstrap = require("bootstrap");
 const express = require("express");
 const app = express();
 const Joi = require("@hapi/joi");
 const movies = require("./movies");
 const path = require("path");
-// app.use(express.json());
+const sassMiddleware = require("node-sass-middleware");
 
-// app.get("/", (req, res) => {
-//   res.send("歡迎來到永夜安安工作室！！探險火寶現正熱映中！！1226");
-// });
+// 設定視圖引擎為 HTML（而非 Pug）
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "html");
+
+app.use(
+  sassMiddleware({
+    src: path.join(__dirname, "scss"), // SCSS 檔案所在的目錄
+    dest: path.join(__dirname, "public"), // 編譯後的 CSS 檔案輸出目錄
+    debug: true,
+    outputStyle: "compressed", // 選擇編譯後的 CSS 格式（壓縮）
+  })
+);
 
 app.use(express.static(path.join(__dirname, "public")));
 
