89118991ce1660a312b7b6466c501311c2a29f06
kulasisin <107702039@mail2.nccu.tw>
2023-12-30 17:00:22 +0800

    add photo

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 const express = require("express");
 const app = express();
 // const Joi = require("@hapi/joi");
-const movies = require("./movies");
+
 const path = require("path");
 const sassMiddleware = require("node-sass-middleware");
 
